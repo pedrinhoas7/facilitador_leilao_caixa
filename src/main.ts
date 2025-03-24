c497ee5ab40b35a5146449a5d22ea0e497c8c89d
pedrinhoas7 <pedrinhoas7@gmail.com>
2025-03-23 21:33:49 -0300

    feat: test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
+
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
